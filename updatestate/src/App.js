f636d46fc5bf264353ed1f4dca9cda96a54e3a46
lolu1032 <soowkw@naver.com>
2025-02-19 23:56:04 +0900

    State 업데이트 큐

---
+++ updatestate/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RequestTracker from './Challnege01/component';
+
+function App() {
+  return (
+    <>
+      <RequestTracker />
+    </>
+  );
+}
+
+export default App;

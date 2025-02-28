31356696a73d9ac1514b69d6d981436d2cb11dd6
lolu1032 <soowkw@naver.com>
2025-02-28 14:44:08 +0900

    Context를 사용해 데이터를 깊게 전달하기

---
+++ contextstate/src/App.js
@@ -0,0 +1,64 @@
+import { useContext, useState } from 'react';
+import { places } from './Challenge01/data.js';
+import { getImageUrl } from './Challenge01/utils.js';
+import { SizeContext } from './Challenge01/context.js';
+
+export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+  return (
+    <SizeContext.Provider
+    value={imageSize}
+    >
+      <label>
+        <input
+          type="checkbox"
+          checked={isLarge}
+          onChange={e => {
+            setIsLarge(e.target.checked);
+          }}
+        />
+        Use large images
+      </label>
+      <hr />
+      <List />
+    </SizeContext.Provider>
+  )
+}
+
+function List() {
+  const listItems = places.map(place =>
+    <li key={place.id}>
+      <Place
+        place={place}
+      />
+    </li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function Place({ place }) {
+  return (
+    <>
+      <PlaceImage
+        place={place}
+      />
+      <p>
+        <b>{place.name}</b>
+        {': ' + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage({ place }) {
+  const imageSize = useContext(SizeContext);
+  return (
+    <img
+      src={getImageUrl(place)}
+      alt={place.name}
+      width={imageSize}
+      height={imageSize}
+    />
+  );
+}

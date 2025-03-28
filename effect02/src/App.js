509b91dc5d007f27b0711af39ad8786f237aa28a
lolu1032 <soowkw@naver.com>
2025-03-28 17:02:16 +0900

    Effect가 필요하지 않은 경우

---
+++ effect02/src/App.js
@@ -0,0 +1,10 @@
+import ContactManager from "./Challenge03/ContactManager";
+import EditContact from "./Challenge03/EditContact";
+
+function App() {
+  return (
+    <ContactManager />
+  );
+}
+
+export default App;

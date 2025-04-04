63b38c73f4d746db187ab25cc670c33d4668629f
lolu1032 <soowkw@naver.com>
2025-04-04 21:44:23 +0900

    Effect에서 이벤트 분리하기

---
+++ effectevent/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Timer from './Challenge01/Timer';
+
+function App() {
+  return (
+    <>
+      <Timer />
+    </>
+  );
+}
+
+export default App;
---
+++ effectevent/src/Challenge03/Timer.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import { experimental_useEffectEvent as useEffectEvent } from 'react';
+
+export default function Timer() {
+  const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
+  const [delay, setDelay] = useState(100);
+
+  const onTick = useEffectEvent(() => {
+    setCount(c => c + increment);
+  });
+
+  const onMount = useEffectEvent(() => {
+    return setInterval(() => {
+      onTick();
+    }, delay);
+  });
+
+  useEffect(() => {
+    const id = onMount();
+    return () => {
+      clearInterval(id);
+    }
+  }, [delay]);
+
+  return (
+    <>
+      <h1>
+        카운터: {count}
+        <button onClick={() => setCount(0)}>재설정</button>
+      </h1>
+      <hr />
+      <p>
+        증가량:
+        <button disabled={increment === 0} onClick={() => {
+          setIncrement(i => i - 1);
+        }}>–</button>
+        <b>{increment}</b>
+        <button onClick={() => {
+          setIncrement(i => i + 1);
+        }}>+</button>
+      </p>
+      <p>
+        증가 지연 시간:
+        <button disabled={delay === 100} onClick={() => {
+          setDelay(d => d - 100);
+        }}>–100 ms</button>
+        <b>{delay} ms</b>
+        <button onClick={() => {
+          setDelay(d => d + 100);
+        }}>+100 ms</button>
+      </p>
+    </>
+  );
+}
